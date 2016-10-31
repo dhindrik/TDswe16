9c6b8cd4ae604f64e5b2b7b89a0bc0724372d00c
Johan Karlsson <johan.kson@gmail.com>
2016-10-31 08:21:45 +0100

    Initial project commit for Lab - Internal messaging

---
+++ Lab - Internal Messaging/lab/InternalMessaging/InternalMessagingPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace InternalMessaging
+{
+	public partial class InternalMessagingPage : ContentPage
+	{
+		public InternalMessagingPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
