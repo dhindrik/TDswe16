2c9e63b74d8203669d8d41a73f7ff159e5a8aa5b
Daniel Hindrikes <daniel@hindrikes.se>
2016-11-14 20:49:33 +0100

    ios lab

---
+++ Lab - Xamarin.iOS/solution/Xamarin_iOS/CreateViewController.cs
@@ -0,0 +1,37 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace Xamarin_iOS
+{
+    public partial class CreateViewController : UIViewController
+    {
+        public CreateViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            this.NavigationItem.SetRightBarButtonItem(
+            new UIBarButtonItem(UIBarButtonSystemItem.Save, (sender, args) => {
+
+                var message = string.Format("Hello {0} {1}", FirstName.Text, LastName.Text);
+
+                var dialog = new UIAlertView()
+                {
+                    Title = "Welcome",
+                    Message = message
+                };
+                dialog.AddButton("OK");
+                dialog.Show();
+
+                var viewController = Storyboard.InstantiateViewController("MainTabs");
+
+                NavigationController.ShowViewController(viewController, this);
+
+            }), true);
+        }
+    }
+}
